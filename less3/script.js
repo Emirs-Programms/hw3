8a66861cdfe862ca12a2edfd73d72038cb2ace11
Emirs-Programms <emirmarash@gmail.com>
2024-05-24 18:43:26 +0600

    Add files via upload

---
+++ less3/script.js
@@ -0,0 +1,25 @@
+
+const array =[
+    { name: "Alice", age: 10, grade: 90 },
+    { name: "Bob", age: 22, grade: 85 },
+    { name: "Charlie", age: 21, grade: 88 },
+    { name: "Alice", age: 30, grade: 90 },
+    { name: "Bob", age: 11, grade: 85 },
+    { name: "Charlie", age: 15, grade: 88 },
+    { name: "Alice", age: 18, grade: 90 },
+    { name: "Bob", age: 12, grade: 85 },
+    { name: "Charlie", age: 41, grade: 88 }
+];
+
+
+const age18 = []
+
+for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if(element.age >= 18){
+        age18.push(element.name)
+    }
+}
+
+console.log(age18);
+
